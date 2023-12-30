0ea39aef323b3aeb135d769003987ae723c00169
Knevari <winresource000@gmail.com>
2023-12-30 17:36:32 -0300

    style: update pink colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: {
+          50: "#fef2f4",
+          100: "#fde6ea",
+          200: "#fbd0da",
+          300: "#f8a9bc",
+          400: "#f37999",
+          500: "#ec5e87",
+          600: "#d62862",
+          700: "#b41c53",
+          800: "#811a46",
+          900: "#811a46",
+          950: "#480922",
+        },
+      },
+    },
   },
   plugins: [],
 };
